7709af317e91b426616dd9fc9600dc1fc8c9b53c
ElmaShabani <154560182+ElmaShabani@users.noreply.github.com>
2024-05-12 22:46:16 +0200

    test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('Setting up the project')
+
+
+def caesar_encrypt(input_text, shift):
+    encrypted_text = ""
+    for char in input_text:
+        if char.isalpha():
+            if char.isupper():
+                encrypted_text += chr((ord(char) + shift - 65) % 26 + 65)
+            else:
+                encrypted_text += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            encrypted_text += char
+    return encrypted_text
