8b70a109d2f27110ffd8e8c750468cc7c4aa250e
ElmaShabani <154560182+ElmaShabani@users.noreply.github.com>
2024-05-13 20:20:17 +0200

    test.py

--- test.py
+++ test.py
@@ -11,3 +11,15 @@ def caesar_encrypt(input_text, shift):
         else:
             encrypted_text += char
     return encrypted_text
+
+def caesar_decrypt(encrypted_text, shift):
+    decrypted_text = ""
+    for char in encrypted_text:
+        if char.isalpha():
+            if char.isupper():
+                decrypted_text += chr((ord(char) - shift - 65) % 26 + 65)
+            else:
+                decrypted_text += chr((ord(char) - shift - 97) % 26 + 97)
+        else:
+            decrypted_text += char
+    return decrypted_text
